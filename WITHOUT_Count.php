c769a58181984b536afbb9e15d432c047f5b71cf
Hussain_ali <165026313+Fidahussain12@users.noreply.github.com>
2024-05-07 12:19:45 -0700

    Add files via upload

---
+++ WITHOUT_Count.php
@@ -0,0 +1,13 @@
+
+<pre>
+<?php
+$array = array("apple", "banana", "cherry", "date", "elderberry");
+$counter = 0;
+
+foreach ($array as $element) {
+    $counter++;
+}
+
+echo "The number of elements in the array is: $counter";
+?>
+</pre>
